56544c8b7363992d80a2f7a7045f47f3af04ae36
Davinder Singh <davindersaini003@gmail.com>
2020-01-30 21:56:41 +0530

    new

--- burger_builder/src/UI/Modal/Modal.js
+++ burger_builder/src/UI/Modal/Modal.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Backdrop from '../Backdrop/Backdrop';
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 
 import classes from './Modal.css';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} closed={props.modalClosed}/>
-        <div className={classes.Modal} 
-        style={{transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
-                opacity: props.show ? 1 : 0}}
-        >{props.children}
-        </div>
-    </Aux>  
-);
+class modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+            return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal.js] componentDidUpdate');
+    }
+
+    render () {
+        return (
+        <Aux>
+            <Backdrop show={this.props.show} closed={this.props.modalClosed}/>
+            <div className={classes.Modal} 
+            style={{transform: this.props.show ? 'translateY(0)': 'translateY(-100vh)',
+                    opacity: this.props.show ? 1 : 0}}
+            >{this.props.children}
+            </div>
+        </Aux>
+        );
+    };
+}
 
 export default modal;
--- burger_builder/src/components/OrderSummary/OrderSummary.js
+++ burger_builder/src/components/OrderSummary/OrderSummary.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary.js] componetDidUpdate')
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(key => {
             return (
             <li key={key}>
-                <span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}
+                <span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}
             </li>)
-        }
-        );
+        });
 
-    return (
+        return (
         <Aux>
             <h3>Your Order</h3>
             <p>A Delicious Burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
         </Aux>
-    );
-};
+        );
+    };
+}
 
-export default orderSummary;
+export default OrderSummary;
