fbf5e9f3b8d80a5b027f406a4fc7d8146a49e165
Davinder Singh <davindersaini003@gmail.com>
2020-02-11 00:13:00 +0530

    new

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionType from '../../store/actions';
 
 class Counter extends Component {
     state = {
@@ -28,14 +30,39 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.incrementCtr} />
+                <CounterControl label="Decrement" clicked={this.props.decreaseCtr}  />
+                <CounterControl label="Add 5" clicked={this.props.addToCtr}  />
+                <CounterControl label="Subtract 5" clicked={this.props.subtractFromCtr}  />
+                <hr /> {/**horizontal line*/}
+                <button onClick={this.props.onStoreResult}>Save Counter</button>
+                <ul>
+                    {this.props.storedResults.map(result => {
+                        return <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>;
+                    })}
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        ctr: state.counter,
+        storedResults: state.results
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        incrementCtr: () => dispatch({type: actionType.INCREMENT}),
+        decreaseCtr: () => dispatch({type: actionType.DECREMENT}),
+        addToCtr: () => dispatch({type: actionType.ADD, value: 5}),
+        subtractFromCtr: () => dispatch({type: actionType.SUBTRACT, value: 5}),
+        onStoreResult: () => dispatch({type: actionType.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionType.DELETE_RESULT, resultElId: id})
+    };
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);   //connect(mapStateToProps, mapActionToProps)
--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,8 +1,44 @@
+import * as actionType from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionType.INCREMENT:
+            const newState = Object.assign({}, state); // remember it will not deeply clone the state and that's ok because we are not altering the result(which is a ref type, so containing a ref to the object in the state) in any way in this case.
+            newState.counter++;
+            return newState;
+        case actionType.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        case actionType.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            };
+        case actionType.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.value
+            };
+        case actionType.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id : new Date().getTime(), value: state.counter}) // concat will return a new array and will not alter the original array where as if had used push it will mutate the original array also
+            };
+        case actionType.DELETE_RESULT:
+            const newResultsArr = state.results.filter(result => result.id !== action.resultElId)
+            return {
+                ...state,
+                results:  newResultsArr
+            }
+    }
+
     return state;
 };
 
